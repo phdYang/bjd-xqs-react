871317391128d56af5e3dcd6290ab560b6c8c815
zhaoyang <ph.d_yang@qq.com>
2018-12-25 16:04:14 +0800

    20181225-4

--- src/pages/ui/buttons/index.js
+++ src/pages/ui/buttons/index.js
@@ -46,7 +46,7 @@ export default class Buttons extends React.Component{
                     <Button shape="circle" loading={this.state.loading}></Button>
                     <Button type="primary" onClick={this.handleCloseLoading}>关闭</Button>
                 </Card>
-                <Card title="按钮组">
+                <Card title="按钮组" style={{marginBottom:10}}>
                     <Button.Group>
                         <Button type="primary" icon="left">返回</Button>
                         <Button type="primary" icon="right">前进</Button>
