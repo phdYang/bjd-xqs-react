806db0a886957a1238d7ee80cfc820b1cc67a6db
phdYang <ph.d_yang@qq.com>
2019-01-06 13:30:13 +0800

    11111

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
-export default class Login extends React.Component{
+export default withRouter(class Login extends React.Component{
 
+    login(){
+        sessionStorage.setItem('login',true)
+        this.props.history.push('/')
+    }
+    
     render(){
         return (
             <div>
                 this is a Login page.
+                {/* <div><Link to="/">login</Link></div> */}
+                <div onClick={() => this.login()}>login</div>
+                
             </div>
         );
     }
-}
+})
