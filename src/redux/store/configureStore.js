d42876cca1b996dcf7b2a6e3bd73d350a781ec34
zhaoyang <ph.d_yang@qq.com>
2019-07-14 21:12:59 +0800

    删除一些文件

--- src/redux/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux'
-import reducer from './../reducer'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-
-const initialState = {
-    menuName: ''
-}
-
-export default () => createStore(reducer, initialState);
